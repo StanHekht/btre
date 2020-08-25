9c72f978e482e2d71288b436591979f0d4a56f8a
Stanislau Hekht <stanislauhekht@Stanislaus-iMac.fios-router.home>
2020-08-24 23:26:46 -0400

    Removed trailing whitespace

--- pages/views.py
+++ pages/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from listings.choices import price_choices, bedroom_choices, state_choices
 
-from listings.models import Listing 
+from listings.models import Listing
 from realtors.models import Realtor
 
 
