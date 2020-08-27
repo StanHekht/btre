0150d7b0848f2d8d6620e67d8d0908177916d19b
Stanislau Hekht <stanislauhekht@Stanislaus-iMac.fios-router.home>
2020-08-27 00:52:39 -0400

    Added user registration logic

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        # Register user
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['first_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # Check username
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That email is being used')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+                    user.save()
+                    messages.success(request, 'You are now registred and can log in')
+                    return redirect('login')
+
+        else:
+            messages.error(request, 'Passwords do not match')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
+    
+    
+
 
 def login(request):
     if request.method == 'POST':
