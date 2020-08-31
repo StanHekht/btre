ab4c0c4d845e8a9b07cc409117b462783b37d9e5
Stanislau Hekht <stanislauhekht@Stanislaus-iMac.fios-router.home>
2020-08-30 21:38:15 -0400

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'hekhtstanislau@gmail.com'
 EMAIL_HOST_PASSWORD =  'slavainus140187'
 EMAIL_USE_TLS = True
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
